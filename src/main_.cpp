edb83b833a0fdd25a218cf7b4fa3aa01fccc6de8
Cahree <cmyrick1@umbc.edu>
2025-09-12 17:26:47 -0400

    added pons

--- src/main_.cpp
+++ src/main_.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+
 
 const int ROWS = 8;
 const int COLS = 8;
@@ -8,14 +10,27 @@ class Piece{
 public:
   std::string color;
   std::string piece_type;
+  std::unordered_map<std::string, char> piece_displays {
+    {"pawn", 'p'},
+    {"kngiht", 'n'},
+    {"bishop", 'b'},
+    {"king", 'k'},
+    {"queen", 'q'},
+    {"rook", 'r'},
+    {"empty", '-'}
+  };
   char display;
+
   
   // defualt constructor (no args)
   Piece() : color("None"), piece_type("Empty"), display('-') {}
 
-  // 3-arg constuctor
-  Piece(std::string color, std::string piece_type, char display) : 
-  color(color), piece_type(piece_type), display(display) {}
+  // 2-arg constuctor
+  Piece(std::string color, std::string piece_type){
+    this->color=color;
+    this->piece_type=piece_type;
+    this->display = piece_displays[piece_type];
+  }
 
 };
 
@@ -25,7 +40,7 @@ class Board{
 
   void set_pawn(std::string color, int row){
     for (int j = 0; j < COLS; j++){
-      Piece p(color, "pawn", 'p');
+      Piece p(color, "pawn");
       board[row][j] = p;
     }
   }
@@ -34,10 +49,41 @@ class Board{
     for (int i = 1; i < 7; i++){
       for (int j = 0; j < 8; j++)
       {
-        Piece p("None", "Empty", '-');
+        Piece p("None", "Empty");
         board[i][j] = p;
       }
     }
+  }
+
+  void set_board(){
+
+    // brute force alg, present all possible combinations and traverse each one
+     std::unordered_map<std::string, int> pawn_presets{
+        {"white", 1},
+        {"black", 6}
+     };
+    
+    // set white pawn
+      for (auto it = pawn_presets.begin(); it != pawn_presets.end(); it++)
+      {
+        for (int j = 0; j < COLS; j++)
+        {
+          Piece p(it->first, "pawn");
+          board[it->second][j] = p;
+        }
+      }
+
+      /*
+      // set minor pieces
+      Piece p("white", "knight");
+      board[0][1] = p;
+
+      Piece p("white", "kngiht");
+      board[0][6] = p;
+
+      Piece p("black", "knight");
+      board[7][1] = p;
+      */
   }
 
   void display_board(){
@@ -59,14 +105,31 @@ class DayofYear{
 
 class Game{
 public:
-  // Game() : {}
+
+  void user_interaction(){
+    std::cout << "----- WELCOME TO CHESSMANIA -----" << std::endl;
+    std::cout << "What game mode would you like to play?" << std::endl;
+    std::cout << "1. Player vs. Player (PVP)" << std::endl;
+    std::cout << "2. Player vs AI Bot (PVB)" << std::endl;
+    std::cout << "3. Player vs Creator (PVC)" << std::endl;
+  }
+
+  void start_game(){
+    // create and set board
+    Board b;
+    b.set_board();
+    b.display_board();
+
+  }
+
+  
 
 };
 
 int main()
 {
-  Board b;
-  b.display_board();
+  Game game;
+  game.start_game();
 
   return 0;
 }
