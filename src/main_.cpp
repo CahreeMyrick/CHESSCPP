8f258627c3c40f1e8766ae53a4619251bc6118e8
Cahree <cmyrick1@umbc.edu>
2025-09-12 21:01:06 -0400

    finished board set up

--- src/main_.cpp
+++ src/main_.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
-
+#include <cctype>
 
 const int ROWS = 8;
 const int COLS = 8;
@@ -12,7 +12,7 @@ public:
   std::string piece_type;
   std::unordered_map<std::string, char> piece_displays {
     {"pawn", 'p'},
-    {"kngiht", 'n'},
+    {"knight", 'n'},
     {"bishop", 'b'},
     {"king", 'k'},
     {"queen", 'q'},
@@ -21,7 +21,6 @@ public:
   };
   char display;
 
-  
   // defualt constructor (no args)
   Piece() : color("None"), piece_type("Empty"), display('-') {}
 
@@ -29,7 +28,7 @@ public:
   Piece(std::string color, std::string piece_type){
     this->color=color;
     this->piece_type=piece_type;
-    this->display = piece_displays[piece_type];
+    this->display = (color=="black") ? piece_displays[piece_type] : (piece_displays[piece_type] - ('a' - 'A'));
   }
 
 };
@@ -38,52 +37,36 @@ class Board{
   public:
   Piece board[ROWS][COLS];
 
-  void set_pawn(std::string color, int row){
-    for (int j = 0; j < COLS; j++){
-      Piece p(color, "pawn");
-      board[row][j] = p;
-    }
-  }
-
-  void set_empty(){
-    for (int i = 1; i < 7; i++){
-      for (int j = 0; j < 8; j++)
-      {
-        Piece p("None", "Empty");
-        board[i][j] = p;
-      }
-    }
-  }
-
   void set_board(){
-
-    // brute force alg, present all possible combinations and traverse each one
-     std::unordered_map<std::string, int> pawn_presets{
-        {"white", 1},
-        {"black", 6}
-     };
-    
-    // set white pawn
-      for (auto it = pawn_presets.begin(); it != pawn_presets.end(); it++)
+    std::vector<std::string> colors = {"white", "black"};
+    for (std::vector<std::string>::iterator color = colors.begin(); color != colors.end(); color++)
+    {
+      // set pawns
+      for (int j = 0; j < COLS; j++)
       {
-        for (int j = 0; j < COLS; j++)
-        {
-          Piece p(it->first, "pawn");
-          board[it->second][j] = p;
-        }
+        board[(*color=="black" ? 1: 6)][j] = Piece(*color, "pawn");
       }
 
-      /*
-      // set minor pieces
-      Piece p("white", "knight");
-      board[0][1] = p;
+      // set knights, bishops, rooks
+      for (int i = 0; i < 2; i++)
+      {
+       //knight
+        board[(*color=="black") ? 0 : 7][(i==0) ? 1 : 6] = Piece(*color, "knight");
 
-      Piece p("white", "kngiht");
-      board[0][6] = p;
+       //bishop
+        board[(*color=="black") ? 0 : 7][(i==0 ? 2 : 5)] = Piece(*color, "bishop");
+      
+        //rook
+        board[(*color=="black") ? 0 : 7][(i==0 ? 0 : 7)] = Piece(*color, "rook");
+      
+      }
 
-      Piece p("black", "knight");
-      board[7][1] = p;
-      */
+      // set king and queen
+      board[(*color=="black") ? 0 : 7][3] = Piece(*color, "queen");
+      board[(*color=="black") ? 0 : 7][4] = Piece(*color, "king");
+
+    }
+
   }
 
   void display_board(){
